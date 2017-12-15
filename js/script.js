d6bffb87308fd559e2ec80842a1894e1b7284c53
ammoradi <amirmohammad.moradi@gmail.com>
2017-12-15 05:33:50 +0330

    top of portfolio implemented

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var myName = new Typed('#myName', {
+    strings: ["Amir Mohammad Moradi"],
+    loop: false,
+    typeSpeed: 100
+});
+
+var myWork = new Typed('#myWork', {
+    strings: ["Hardware Engineering Student", "Web Designer", "Front-End Developer"],
+    loop: false,
+    typeSpeed: 100,
+    startDelay: 2800
+});
